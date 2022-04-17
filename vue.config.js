716056ea1b23b9b61eaffbc12d627b1bb104adf7
Yudai Watanabe <yudai.0702wa@gmail.com>
2022-04-17 19:35:35 +0900

    vuetifyのひな形

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ]
-})
+module.exports = {
+  devServer: {
+    disableHostCheck: true,
+  },
+
+  transpileDependencies: ['vuetify'],
+}
